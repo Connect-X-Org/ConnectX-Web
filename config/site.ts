3ce1241fb3fe7e1d7a4e629bf0fbbd0fab636ed3
Connect X <connectxrw@gmail.com>
2025-10-22 11:30:51 +0200

    chnaged site url

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
   name: "ConnectX",
-  url: "https://gateway-rw.vercel.app/", // Replace with production URL
-  ogImage: "https://gateway-rw.vercel.app/opengraph-image.png",
+  url: "https://connectx-beta.vercel.app/", // Replace with production URL
+  ogImage: "https://connectx-beta.vercel.app/opengraph-image.png",
   description:
     "Explore, compare, and connect with service providers in Rwanda effortlessly.",
 };
