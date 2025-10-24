19b1ed847d9ad9977b4df877c54fb05ef02ea9c5
Connect X <connectxrw@gmail.com>
2025-10-24 17:32:48 +0200

    changed not found page

--- app/[...not_found]/page.tsx
+++ app/[...not_found]/page.tsx
@@ -1,35 +1,30 @@
-import { SearchIcon } from "lucide-react";
-import { CommandMenu } from "@/components/layout/command-menu";
-import {
-  Empty,
-  EmptyContent,
-  EmptyDescription,
-  EmptyMedia,
-  EmptyTitle,
-} from "@/components/ui/empty";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/icon.png";
 export default function NotFound() {
   return (
-    <div className="mx-auto flex h-screen max-w-5xl flex-col items-center justify-center">
-      <Empty className="flex flex-col items-center justify-center text-center">
-        <div className="flex flex-col items-center justify-center gap-6 text-center">
-          <EmptyMedia variant="icon">
-            <SearchIcon />
-          </EmptyMedia>
-          <EmptyTitle className="text-center font-bold text-2xl lg:text-4xl">
-            404 - Not Found
-          </EmptyTitle>
-          <EmptyDescription className="text-center text-xl">
-            The page you&apos;re looking for doesn&apos;t exist. Try searching
-            for what you need below.
-          </EmptyDescription>
+    <section className="relative flex min-h-screen items-center justify-center">
+      <div className="relative flex h-full flex-col items-center justify-center bg-white text-black dark:bg-black dark:text-white">
+        <div className="relative mb-8">
+          <Image
+            alt="logo"
+            className="size-10 rounded-lg"
+            height={40}
+            src={logo}
+            width={40}
+          />
         </div>
-        <EmptyContent className="flex flex-col items-center justify-center text-center">
-          <CommandMenu />
-          <EmptyDescription className="text-center">
-            Need help? <a href="/">Contact support</a>
-          </EmptyDescription>
-        </EmptyContent>
-      </Empty>
-    </div>
+        <h1 className="font-normal text-8xl">404</h1>
+        <p className="mb-8 text-sm">Need help? Visit the docs</p>
+        <div className="flex flex-col items-center gap-6">
+          <Link
+            className="border-2 border-black bg-white px-4 py-1.5 text-black text-sm uppercase shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] transition duration-200 hover:shadow-sm md:px-8 dark:border-stone-100 dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] dark:hover:shadow-sm"
+            href="/"
+          >
+            Go to home page
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
