7f30989a3aeaa9493e7322b8e306db193fa4f428
Connect X <connectxrw@gmail.com>
2025-11-02 20:30:50 +0200

    removed unwanted ai features

--- app/(ai-page)/ai/page.tsx
+++ app/(ai-page)/ai/page.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Suspense } from "react";
-import Chat from "@/features/ai/chat";
+import ChatSection from "@/features/ay/chat-section";
 export const metadata: Metadata = {
   title: "Ai",
 };
 export default function AiPage() {
   return (
     <Suspense>
-      <Chat />
+      <ChatSection />
     </Suspense>
   );
 }
--- app/(ai-page)/ay/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { Metadata } from "next";
-import ChatSection from "@/features/ay/chat-section";
-export const metadata: Metadata = {
-  title: "Ai",
-};
-export default function AiPage() {
-  return <ChatSection />;
-}
