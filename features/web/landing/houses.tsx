f92a203290b7556c413568f5399dac96ee26c4bf
Connect X <connectxrw@gmail.com>
2025-11-16 15:55:43 +0200

    removed container

--- features/web/landing/houses.tsx
+++ features/web/landing/houses.tsx
@@ -8,7 +8,7 @@ import HousesGrid from "../housing/houses-grid";
 export default function Houses() {
   return (
     <section className="container py-16 lg:py-20">
-      <div className="container relative flex flex-col gap-6 lg:gap-12">
+      <div className="relative flex flex-col gap-6 lg:gap-12">
         <div className="flex items-center justify-between gap-4">
           <h2 className="text-center font-semibold text-2xl capitalize">
             <Highlighter action="underline" color="#FF9800">
