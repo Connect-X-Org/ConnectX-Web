78e113208f5ade5326db0df119881167a11ba644
Connect X <connectxrw@gmail.com>
2025-11-16 13:52:04 +0200

    fixed restaurant

--- features/web/landing/restaurants.tsx
+++ features/web/landing/restaurants.tsx
@@ -7,7 +7,7 @@ import RestaurantsGrid from "../restaurants/restaurants-grid";
 export default function Restaurants() {
   return (
     <section className="container py-16 lg:py-20">
-      <div className="relative flex flex-col gap-6 lg:gap-12">
+      <div className="@container relative flex flex-col gap-6 lg:gap-12">
         <div className="flex items-center justify-between gap-4">
           <h2 className="text-center font-semibold text-2xl capitalize">
             <Highlighter action="underline" color="#FF9800">
